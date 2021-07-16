138e3bd15ca74a7271fdc1371f2d89e513947ced
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 11:36:45 -0400

    created helper methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,17 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content, :recipe_id
   end
-  
+
+  def set_comment
+    @comment = Comment.find_by(id: params[:id])
+    if !@comment
+      flash[:message] = "Comment was not found"
+      redirect_to comments_path
+    end
+  end
+
+  def redirect_if_not_comment_author
+    redirect_to comments_path if @comment.user != current_user
+  end
+
 end
