3ac6ffb1b7bf22eb2cf893ca345c0aa23b13d8f7
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-15 23:51:28 -0400

    updated shema user -> username

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_014612) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
