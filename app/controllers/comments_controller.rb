f2cb1e58852c6b914df6653cbca9cfc8ef766248
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 12:22:58 -0400

    fixed missing parenthesis

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,7 +47,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :recipe_id
+    params.require(:comment).permit(:content, :recipe_id)
   end
 
   def set_comment
