bccf2fd8c726906506f7247b385ed9df99012b59
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 10:25:08 -0400

    before action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,11 @@
 class RecipesController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :ingredients, :instructions, category_attributes: [:name])
+  end
 end
