d1144b38f86b70be49b5ed2c1f6df182eb849ccf
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 10:34:12 -0400

    created new and index actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,26 @@
 class RecipesController < ApplicationController
   before_action :redirect_if_not_logged_in
 
-  
+  def new
+    if params[:user_id] && @user = User.find_by_id(params[:user_id])
+      @recipe = @user.recipes.build
+    else
+      @recipe = Recipe.new
+    end
+    #build category
+  end
+
+  def index 
+    if params[:user_id] && @user = User.find_by_id(params[:user_id])
+      @recipes = @user.recipes
+    else
+      #error
+      @recipes = Recipe.includes(:category, :user)
+    end
+    @recipes = Recipe.search(params[:q].downcase) if params[:q] && !params[:q].empty?
+  end
+
+
 
   private
 
