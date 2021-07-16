fe7a42124a59d9850dc40b6056259addf9f0ba08
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-15 22:00:21 -0400

    created user model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :recipes
+  has_many :categories, through: :recipes
+  has_many :comments
+  has_many :commented_recipes, through: :comments, source: :recipe # The odin project
+  has_secure_password # Authenticate, validate passowrd & password confirmation
+
+  validates :user, :email, presence: true, uniqueness: true 
 end
