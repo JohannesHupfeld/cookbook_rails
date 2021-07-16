db8571cb5e22b92bb5de22407c346e6b650b0f42
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 13:51:03 -0400

    log in wtih good action and private helper method auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,11 +17,31 @@ class SessionsController < ApplicationController
     end
   end
 
+   #login with google
+   def google
+    #find_or_create a user using the attributes auth
+    @user = User.find_or_create_by(email: auth["info"]["email"]) do |user| #find user binding pry --auth
+      user.username = auth["info"]["first_name"]
+      user.password = SecureRandom.hex(10) #generates random password -- does not use their actual google password
+    end
+    if @user #.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
   #logout
   def destroy
     session.clear
     redirect_to root_path
   end
   
+  private
+
+  def auth
+    request.env['omniauth.auth']
+  end
 
 end
