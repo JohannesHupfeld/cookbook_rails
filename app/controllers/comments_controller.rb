34eafb9da020e44b36892cfab35d5fe658ccbb93
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 11:39:12 -0400

    created create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,15 @@ class CommentsController < ApplicationController
     end
   end
 
+  def create
+    @comment = current_user.comments.build(comment_params)
+    if @comment.save
+      redirect_to comments_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def comment_params
