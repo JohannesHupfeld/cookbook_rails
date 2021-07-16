a4af7a1e1b1d2040c52f16ad7ae04c5105fc5c1c
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 10:38:47 -0400

    created scope alpha and search helper

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,13 @@ class Recipe < ApplicationRecord
   has_many :users, through: :comments # Gives us the writer method for the plural 
   validates :name, :instruction, :ingredient, presence: true
  
+  scope :alpha, -> { order(:name) }
+
+  def self.search(params)
+    left_joins(:comments).where("LOWER(recipes.name) LIKE :term OR LOWER(recipes.ingredient) LIKE :term", term: "%#{params}%")
+    #select any recipe where the name or ingredients closely match search 
+  end
+
   #accepts_nested_attributes_for :categories --this macro doesnt work when has_many
   def category_attributes=(attr) # Necessary to define yourself when we have a has_many
     self.category = Category.find_or_create_by(attr) if !attr[:name].blank?
