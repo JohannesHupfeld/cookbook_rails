19b84a4f4aafa7faf4eb7ad64887e9a7f3ebe52b
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-16 10:19:57 -0400

    added notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      #add flash message
       redirect_to login_path
     end
   end
