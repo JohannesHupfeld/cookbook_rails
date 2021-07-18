4731c60562548875c6b86e721027b056fbc53658
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-18 11:11:10 -0400

    added notes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  #signup 
   def create 
     @user = User.new(user_params)
     if @user.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :categories, through: :recipes
   has_many :comments
-  has_many :commented_recipes, through: :comments, source: :recipe # The odin project
+  has_many :commented_recipes, through: :comments, source: :recipe # The odin project --active record associations 
   has_secure_password # Authenticate, validate passowrd & password confirmation
 
   validates :email, presence: true, uniqueness: true 
