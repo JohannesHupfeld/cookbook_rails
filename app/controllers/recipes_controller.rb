87eb044d3bab9adccc14fefe8c0ab7fe574b58eb
JohannesHupfeld <j.hupfeld@gmail.com>
2021-07-18 17:42:40 -0400

    created two helper methods and one beofre action to dry code

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,9 @@
 class RecipesController < ApplicationController
   before_action :redirect_if_not_logged_in
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
   def new
-    if params[:user_id] && @user = User.find_by_id(params[:user_id])
+    if find_user_nested
       @recipe = @user.recipes.build
     else
       @recipe = Recipe.new
@@ -11,7 +12,7 @@ class RecipesController < ApplicationController
   end
 
   def index 
-    if params[:user_id] && @user = User.find_by_id(params[:user_id])
+    if find_user_nested
       @recipes = @user.recipes
     else
       @error = "This user doesn't exist" if params[:user_id]
@@ -31,18 +32,15 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.find_by_id(params[:id])
     redirect_to recipes_path if !@recipe
   end
 
   def edit
-    @recipe = Recipe.find_by_id(params[:id])
     redirect_to recipes_path if !@recipe || !authorized_to_edit?
     @recipe.build_category if !@recipe.category
   end
 
   def update
-    @recipe = Recipe.find_by(id: params[:id])
     redirect_to recipes_path if !@recipe || !authorized_to_edit?
     if @recipe.update(recipe_params)
       flash[:message] = "Recipe successfully updated"
@@ -53,7 +51,6 @@ class RecipesController < ApplicationController
   end
 
   def destroy
-    @recipe = Recipe.find_by(id: params[:id])
     if current_user
       @recipe.destroy
       flash[:message] = "Recipe successfully deleted"
@@ -67,5 +64,13 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :ingredients, :instructions, category_attributes: [:name])
+  end
+
+  def find_recipe
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def find_user_nested
+    params[:user_id] && @user = User.find_by_id(params[:user_id])
   end
 end
